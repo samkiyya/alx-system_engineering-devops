e0863bd8bd0e8283722a3579edcab08ea9ed4b46
samkiyya <samuelkiyya2012@gmail.com>
2024-08-13 21:17:54 +0300

    import alphabet corrected

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,42 +3,40 @@
 
 import requests
 
-def count_words(subreddit, word_list, word_count=None, after=None):
+
+def count_words(subreddit, word_list, word_count={}, after=None):
     """Recursively queries the Reddit API and returns the count of words in
     word_list in the titles of all the hot posts of the subreddit."""
+    
+    headers = {"User-Agent": "My-User-Agent"}
+    params = {"after": after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
 
-    if word_count is None:
-        word_count = {}
-
-    sub_info = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        params={"after": after},
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False
-    )
-
-    if sub_info.status_code != 200:
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+    if response.status_code != 200:
         return None
 
-    info = sub_info.json()
-    hot_l = [child.get("data").get("title") for child in info.get("data").get("children")]
+    data = response.json().get("data", {})
+    posts = data.get("children", [])
 
-    word_list = list(dict.fromkeys(word.lower() for word in word_list))
+    if not posts:
+        return None
 
-    for title in hot_l:
-        split_words = title.split()
+    for post in posts:
+        title = post.get("data", {}).get("title", "").lower()
         for word in word_list:
-            word_count[word] = word_count.get(word, 0) + sum(
-                1 for s_word in split_words if s_word.lower() == word)
+            word_lower = word.lower()
+            word_count[word_lower] = word_count.get(word_lower, 0)
+            word_count[word_lower] += title.split().count(word_lower)
 
-    after = info.get("data").get("after")
-    if after is not None:
+    after = data.get("after")
+    if after:
         return count_words(subreddit, word_list, word_count, after)
-
+    
     if word_count:
-        sorted_counts = sorted(word_count.items(), key=lambda kv: (-kv[1], kv[0]))
-        for k, v in sorted_counts:
-            if v > 0:
-                print('{}: {}'.format(k, v))
+        sorted_words = sorted(word_count.items(), key=lambda kv: ( - kv[1], kv[0]))
+        for word, count in sorted_words:
+            if count > 0:
+                print(f"{word}: {count}")
 
     return word_count

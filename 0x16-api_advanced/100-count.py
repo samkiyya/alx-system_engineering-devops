37a08ff980cc4c503490f9e378235423eb05c27c
samkiyya <samuelkiyya2012@gmail.com>
2024-08-13 20:43:30 +0300

    import alphabet corrected

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Module for task 3"""
+import requests
 
 
 def count_words(subreddit, word_list, word_count={}, after=None):
     """Queries the Reddit API and returns the count of words in
     word_list in the titles of all the hot posts
     of the subreddit"""
-    import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
                             .format(subreddit),
